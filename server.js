79722d241c8e69e40f69940ab8d752c683508b97
wesley-gdd <wesley@good.dog>
2018-08-07 14:07:48 -0700

    update listen

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ var app = require('./server/index');
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+  console.log('Express server listening on port ' + this.address().port, app.settings.env);
 });
